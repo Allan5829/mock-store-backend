9e9c95403c674c613f13786f331106758c040d45
Allan5829 <allan.farca@gmail.com>
2021-01-16 12:13:41 -0800

    edits products controller create action and adds product params.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,8 +8,23 @@ class ProductsController < ApplicationController
     end 
 
     def create
+        product = Product.new(product_params)
+
+        if product.save
+            render json: product
+        else
+            render json: product.errors
+        end
     end 
 
     def destroy
     end 
+
+    private
+
+    def product_params
+      params.require(:product).permit(
+          :name, :price, :size, :color, :main_category, :sub_category, :gender, :image
+        )
+    end
 end
