dfab302b0630d87df129bffb37fcbc8346b3bbbc
Allan5829 <allan.farca@gmail.com>
2021-01-24 01:27:20 -0800

    adds functionality to the show and destroy controller actions and creates a function to trigger before the respective actions.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,14 @@
 class ProductsController < ApplicationController
+
+    before_action :find_product, only: [:show, :destroy]
+
     def index
         products = Product.all
         render json: products
     end
 
     def show
+        render json: @product
     end 
 
     def create
@@ -18,9 +22,14 @@ class ProductsController < ApplicationController
     end 
 
     def destroy
+        @product.destroy
     end 
 
     private
+
+    def find_product
+        @product = Product.find(params[:id])
+    end
 
     def product_params
       params.require(:product).permit(
